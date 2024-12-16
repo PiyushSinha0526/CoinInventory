0b7beb2a5a41fba7338949b7ba1a8fc18cd055e4
Piyush Sinha <piyushsinha0526@gmail.com>
2024-12-16 22:07:27 +0530

    feat: clerk auth

--- .next/static/chunks/app_layout_tsx_61af54._.js
+++ .next/static/chunks/app_layout_tsx_61af54._.js
@@ -5,8 +5,10 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_layout_tsx_61af54._.js",
   "chunks": [
-    "static/chunks/[root of the server]__9d1267._.css",
-    "static/chunks/app_layout_tsx_83fc40._.js"
+    "static/chunks/[root of the server]__33bd1d._.css",
+    "static/chunks/node_modules_243d0b._.js",
+    "static/chunks/app_layout_tsx_83fc40._.js",
+    "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_dcdf1d._.js"
   ],
   "source": "dynamic"
 });
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-blue-50 py-12">
+      <div className="container mx-auto px-4 text-center text-gray-600">
+        <p>Made with love by </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
