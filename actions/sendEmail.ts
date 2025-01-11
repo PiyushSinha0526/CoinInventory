358e2ee1b6afbc4de83dbf8c9273b5e4cc84a8db
Piyush Sinha <piyushsinha0526@gmail.com>
2025-01-11 10:32:45 +0530

    feat: budget alert email cron job

---
+++ actions/sendEmail.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { Resend } from "resend";
+import React from "react";
+
+interface SendEmailOptions {
+  to: string;
+  subject: string;
+  react: React.ReactElement;
+}
+
+interface SendEmailSuccess {
+  success: true;
+  data: any;
+}
+
+interface SendEmailFailure {
+  success: false;
+  error: any;
+}
+
+type SendEmailResponse = SendEmailSuccess | SendEmailFailure;
+
+export async function sendEmail({
+  to,
+  subject,
+  react,
+}: SendEmailOptions): Promise<SendEmailResponse> {
+  const resend = new Resend(process.env.RESEND_API_KEY || "");
+  try {
+    const data = await resend.emails.send({
+      from: "CoinInventory <onboarding@resend.dev>",
+      to,
+      subject,
+      react,
+    });
+
+    return { success: true, data };
+  } catch (error) {
+    console.error("Failed to send email:", error);
+    return { success: false, error };
+  }
+}
