366a737530350d5e9327671eb85a7a437005f139
Piyush Sinha <piyushsinha0526@gmail.com>
2025-01-19 22:24:23 +0530

    feat: gemini ai recipt scan from image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
     domains: ["randomuser.me"],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
